8fb3d0a13dfd0929c6f5593d9d93bdc7da325ba6
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-05-22 21:34:47 -0400

    Game Play UI / Scss Setup Fixes #8

---
+++ godfield/src/components/ActionArea/ActionArea.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../shared/Header/Header';
+import { Footer } from '../shared/Footer/Footer';
+import './ActionArea.css';
+
+export const ActionArea = () => {
+	return (
+		<div className="action-area">
+			<Header />
+			<Footer />
+		</div>
+	);
+};
---
+++ godfield/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ActionArea } from '../ActionArea/ActionArea';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="app">
+			<ActionArea />
+		</div>
+	);
+};
+
+export default App;
